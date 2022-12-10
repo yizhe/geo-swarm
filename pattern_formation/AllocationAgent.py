ab5874e9c2b28fde8939d0796b902d830fe03175
raywang4 <57317640+raywang4@users.noreply.github.com>
2022-12-10 16:11:07 -0500

    Add files via upload

--- pattern_formation/AllocationAgent.py
+++ pattern_formation/AllocationAgent.py
@@ -11,11 +11,8 @@ class AllocationAgent(Agent):
 		super().__init__(id, vertex)
 		self.pattern = pattern
 		self.target = None
-		self.msgs = []
-
-	def broadcast(self, msg):
-		#TODO: broadcasts msg (tuple of target distance and target id) to surrounding neighbors
-		pass
+		self.taken = {}
+		self.finished = 0
 
 	def check_conflict(self, msg):
 		for i in self.msgs:
@@ -25,37 +22,59 @@ class AllocationAgent(Agent):
 
 	def generate_transition(self,local_vertex_mapping):
 		nearby_agents = []
+		ownid = 0
 		for dx, dy in local_vertex_mapping.keys():
 			vertex = local_vertex_mapping[(dx,dy)]
 			for agent in vertex.agents:
-				if self != agent:
-					nearby_agents.append(agent)
+				nearby_agents.append(agent)
+				if self == agent:
+					ownid = len(nearby_agents)-1
 		dists = []
+		occupied = {}
+		assigned = {}
 		for id, i in enumerate(self.pattern):
-			d = min(abs(self.location.x-i.location.x), 50-abs(self.location.x-i.location.x))
-			d += min(abs(self.location.y-i.location.y), 50-abs(self.location.y-i.location.y))
-			dists.append((d, id))
+			if id in occupied.keys() or id in self.taken.keys():
+				continue
+			for idx, agent in enumerate(nearby_agents):
+				if idx in assigned.keys():
+					continue
+				if agent.finished:
+					self.taken[agent.finished] = True
+					continue
+				d = abs(agent.location.x-i[0]) + abs(agent.location.y-i[1])
+				if d == 0:
+					occupied[id] = idx
+					assigned[idx] = id
+					self.taken[id] = True
+					if idx == ownid:
+						self.finished = id
+					break
+		if self.finished:
+			return self.location.state, self.state, None
+		for id, i in enumerate(self.pattern):
+			if (id in occupied.keys()) or (id in self.taken.keys()):
+				continue
+			for idx, agent in enumerate(nearby_agents):
+				if idx in assigned.keys() or agent.finished:
+					continue
+				d = abs(agent.location.x-i[0]) + abs(agent.location.y-i[1])
+				dists.append((d, id, idx, i[0], i[1]))
+
 		dists = sorted(dists)
-		now = -1
-		self.target = None
-		while self.target is None:
-			now = now + 1
-			if now >= len(dists):
-				raise IndexError
-			self.broadcast(dists[now])	
-			if not self.check_conflict(dists[now]):
-				self.target = self.pattern[dists[now][1]]
+
+		for i in dists:
+			if (i[1] not in occupied.keys()) and (i[2] not in assigned.keys()):
+				occupied[i[1]] = i[2]
+				assigned[i[2]] = i[1]
+		self.target = self.pattern[assigned[ownid]]
 		
-		best_moves = []
+		best_move = None
 		best_distance = 100000
 		for direction in directions:
 			i = get_coords_from_movement(self.location.x, self.location.y, direction)
-			dist = min(abs(self.target.location.x-i.location.x), 50-abs(self.target.location.x-i.location.x))
-			dist += min(abs(self.target.location.y-i.location.y), 50-abs(self.target.location.y-i.location.y))
+			dist = abs(self.target[0]-i[0])+abs(self.target[1]-i[1])
 			if dist < best_distance:
 				best_distance = dist
-				best_moves = [direction]
-			if dist == best_distance:
-				best_moves.append(direction)
+				best_move = direction
 		#best_moves.append("S")
-		return self.location.state, self.state, random.choice(best_moves)
+		return self.location.state, self.state, best_move
